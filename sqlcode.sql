373afa3268816c279011d12fad4f9ff0a9e5f4cd
tamil368 <tamilaathi2020@gmail.com>
2024-08-08 11:06:37 +0530

    feat: updated

--- sqlcode.sql
+++ sqlcode.sql
@@ -4,4 +4,10 @@ create databse smart_car_system;
 
 --#signup table creation--
 
- --#signup users buyer and seller table creation
+CREATE TABLE users ( id int(3) AUTO_INCREMENT , 
+name VARCHAR(100) NOT NULL, 
+email VARCHAR(255) UNIQUE NOT NULL,
+ phone VARCHAR(15) NOT NULL,
+  password VARCHAR(255) NOT NULL,
+   user_type VARCHAR(10) NOT NULL,
+    PRIMARY KEY (id) );
